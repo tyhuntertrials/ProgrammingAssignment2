0ba3c65961981b876e1a69f5aa875705e66ca630
tyhuntertrials <tyhuntertrials@gmail.com>
2020-09-03 10:16:52 -0700

    Added more comments to make it clear.

--- cachematrix.R
+++ cachematrix.R
@@ -1,38 +1,34 @@
-## Put comments here that give an overall description of what your
-## functions do
+## Creates a cache based on a matrix, where the inverse of that matrix can be stored and then inverses that matrix.
 
-## Write a short comment describing this function
+## Creates a matrix that can cache it's inverse
 
 makeCacheMatrix <- function(x = matrix()) {
-  inv <- NULL
-  set <- function (y){
-    x <<- y
-    inv <<- NULL
+  inv <- NULL #Providing inv variable that will hold inverse matrix
+  set <- function (y){ 
+    x <<- y   #Sets new value of matrix from parent function
+    inv <<- NULL #Sets inv to null when new matrix is present
   }
-  get <- function() x
-  setInverse <- function(inverse){
-    inv <<- inverse
-  }
-  getInverse <- function(){
-    inv
-  }
-  list(set = set, get = get, setInverse = setInverse, getInverse = getInverse)
+  get <- function() x #get function returns matrix
+  setInverse <- function(inverse) inv <<- inverse #Sets value of inv to inverse of matrix
+  getInverse <- function()inv #Retrieves inv
+  list(set = set, get = get, 
+       setInverse = setInverse, 
+       getInverse = getInverse) #Establishing list so that values can be retrieved
 
 }
 
 
-## Write a short comment describing this function
+## Returns a matrix that is the inverse of 'X'
 
 cacheSolve <- function(x, ...) {
-        ## Return a matrix that is the inverse of 'x'
-  inv <- x$getInverse()
-  if(!is.null(inv)){
+  inv <- x$getInverse() #Get inverse and stores in inv
+  if(!is.null(inv)){ # if there is a value in inv then message is displayed and inv is returned
     message("getting cached data")
     return(inv)
   }
-  mat <- x$get()
-  inv <- solve(mat, ...)
-  x$setInverse(inv)
-  inv
+  mat <- x$get() #Calls upon the get function above
+  inv <- solve(mat, ...) #Inputs inverse of matrix into inv
+  x$setInverse(inv) #Makes inv value the inverse
+  inv #displays inv
 }
 
